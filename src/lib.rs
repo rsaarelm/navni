3aea91cae5c9a26003e183810639ffc0567628ce
Risto Saarelma <risto.saarelma@iki.fi>
2023-06-27 08:58:58 +0300

    Higher max update limit

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub const FRAME_DURATION: Duration = Duration::from_micros(1_000_000 / 60);
 
 /// Never ask more than this many updates to prevent death spirals if
 /// something in the update slows things down very badly.
-pub const MAX_UPDATES_PER_FRAME: u32 = 10;
+pub const MAX_UPDATES_PER_FRAME: u32 = 30;
 
 #[cfg_attr(feature = "gui", path = "miniquad/mod.rs")]
 #[cfg_attr(feature = "tty", path = "crossterm/mod.rs")]
